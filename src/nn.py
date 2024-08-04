1a61fb956f9b7e0b5eb814815bd33932ae379030
Reuben Renshaw <reubenrenshaw3@gmail.com>
2024-08-04 02:13:04 -0500

    Added a Third Layer

--- src/nn.py
+++ src/nn.py
@@ -1,19 +1,13 @@
 import sys
 import numpy as np
+import matplotlib
 
-<<<<<<< HEAD
 np.random.seed(0)
-=======
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
->>>>>>> f34728f32cfdabb110351488faafaa30091efa39
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-<<<<<<< HEAD
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
@@ -26,26 +20,3 @@ layer1.forward(X)
 layer2 = Layer_Dense(5, 2)
 layer2.forward(layer1.output)
 print(layer2.output)
-=======
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_outputs = np.dot(inputs , np.array(weights).T) + biases
-
-print(layer1_outputs)
-
-layer2_outputs = np.dot(layer1_outputs , np.array(weights2).T )+ biases2
-
-print(layer2_outputs)
-
->>>>>>> f34728f32cfdabb110351488faafaa30091efa39
