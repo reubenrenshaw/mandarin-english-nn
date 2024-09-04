553474275131e682effd589c8ff299208e0724d5
Reuben Renshaw <reubenrenshaw3@gmail.com>
2024-09-04 18:43:37 -0400

    implemented loss with cross entropy

--- src/nn.py
+++ src/nn.py
@@ -23,6 +23,21 @@ class Activation_SoftMax:
         exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
         probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
         self.output = probabilities
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+class Loss_Categorical_Cross_Entropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 X, y = cd.create_data(100, 3)
 
@@ -35,3 +50,6 @@ activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 print(activation2.output[:5])
+loss_function = Loss_Categorical_Cross_Entropy()
+loss = loss_function.calculate(activation2.output, y)
+print("Loss: " + str(loss))
